e76bcdd649c9e94c0104f7ce07c329580ec980f7
BersainCortes <bersaincortes10@gmail.com>
2025-10-22 03:17:59 -0600

    setup main layout

--- src/layouts/mainLaout.tsx
+++ src/layouts/mainLaout.tsx
@@ -1,3 +1,13 @@
-export default function MainLayout() {
-    return "MainLayout"
+import type { ReactNode } from "react"
+
+type Props = {
+    children?: ReactNode
+}
+
+export default function MainLayout({ children }:Props) {
+    return (
+        <main>
+            { children }
+        </main>
+    )
 }
