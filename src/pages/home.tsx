8a11165c03de9cb0aa10a141cd36dd9b9da9aaa7
BersainCortes <bersaincortes10@gmail.com>
2025-10-22 22:18:20 -0600

    home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,12 @@
+import MainLayout from "../layouts/mainLaout";
+
 export default function Home() {
-    return "Home page"
+    return (
+        <MainLayout>
+            <div>
+                <h1>Bienvenido a HistóricoAPI</h1>
+                <p>Consulta precios históricos de productos de manera gratuita.</p>
+            </div>
+        </MainLayout>
+    )
 }
