97f7cc5bc2f77cdd31f5f83583e26bae8dd8e318
BersainCortes <bersaincortes10@gmail.com>
2025-10-22 22:42:04 -0600

    styles for navbar

--- src/components/navBarComponent.tsx
+++ src/components/navBarComponent.tsx
@@ -2,12 +2,12 @@ import { Link, NavLink } from "react-router-dom"
 
 export default function NavBarComponent() {
     return(
-        <nav>
-            <div>
-                <Link to="/">Historico API</Link>
+        <nav className="fixed w-full top-0 left-0 flex items-center justify-between px-8 py-4 bg-slate-900 text-gray-200 border-b border-slate-700">
+            <div className="font-bold">
+                <Link to="/" >Historico API</Link>
             </div>
 
-            <div>
+            <div className="flex gap-4">
                 <NavLink to="/">Inicio</NavLink>
                 <NavLink to="/">Productos</NavLink>
                 <NavLink to="/">Documentación</NavLink>
