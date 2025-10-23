16ddbe54a31f64905298a9fa05d19c60697f99af
BersainCortes <bersaincortes10@gmail.com>
2025-10-23 00:43:28 -0600

    new path for products page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import './App.css'
 import Home from './pages/home'
+import Products from './pages/products'
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path='/' element={<Home></Home>}></Route>
+                <Route path='/productos' element={<Products></Products>}></Route>
             </Routes>
         </Router>
     )
