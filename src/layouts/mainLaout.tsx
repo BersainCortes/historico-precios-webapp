5574e6e023d1c2c7366f49386b84d6ec2a832c16
BersainCortes <bersaincortes10@gmail.com>
2025-10-22 22:47:07 -0600

    styles for main layout

--- src/layouts/mainLaout.tsx
+++ src/layouts/mainLaout.tsx
@@ -7,14 +7,14 @@ type Props = {
 
 export default function MainLayout({ children }: Props) {
     return (
-        <div>
-            <header>
+        <div className="flex flex-col min-h-screen bg-slate-950 text-gray-200">
+            <header className="z-10">
                 <NavBarComponent></NavBarComponent>
             </header>
-            <main>
+            <main className="grow container mx-auto px-6 py-24">
                 {children}
             </main>
-            <footer>
+            <footer className="border-t border-slate-700 py-6 text-center text-sm text-gray-400">
                 <p> © { new Date().getFullYear() } Historico Precios API </p>
             </footer>
         </div>
