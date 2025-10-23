20ca9240098dcf276d9a8baf04fd1ff5bf04adbd
BersainCortes <bersaincortes10@gmail.com>
2025-10-22 22:08:47 -0600

    navbar component

--- src/components/navBarComponent.tsx
+++ src/components/navBarComponent.tsx
@@ -1,3 +1,17 @@
+import { Link, NavLink } from "react-router-dom"
+
 export default function NavBarComponent() {
-    return "NavBarComponent"
+    return(
+        <nav>
+            <div>
+                <Link to="/">Historico API</Link>
+            </div>
+
+            <div>
+                <NavLink to="/">Inicio</NavLink>
+                <NavLink to="/">Productos</NavLink>
+                <NavLink to="/">Documentación</NavLink>
+            </div>
+        </nav>
+    )
 }
