71d604607f50ee99fc9d246f93fc2c4de9bac834
BersainCortes <bersaincortes10@gmail.com>
2025-10-26 23:56:09 -0600

    centralize base url of api

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-const base_url = axios.create({ baseURL: import.meta.env.API_URL_BASE })
+const base_url = axios.create({
+    baseURL: import.meta.env.VITE_BASE_URL || "https://historico-precios-api.onrender.com",
+});
 
-export default base_url
+export default base_url;
--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,10 +1,38 @@
+import { useState, useEffect } from "react";
+import base_url from "../api/axios";
 import MainLayout from "../layouts/mainLaout";
 
+type Producto = {
+    id: number;
+    nombre: string;
+    unidad: string;
+};
+
 export default function Products() {
-    return(
+    const [productos, setProductos] = useState<Producto[]>([]);
+
+    useEffect(() => {
+        async function fetchProductos() {
+            try {
+                const response = await base_url.get("/productos");
+                setProductos(response.data);
+                console.log(response.data);
+            } catch (error) {
+                console.error("Error al obtener los productos:", error);
+            }
+        }
+
+        fetchProductos();
+    }, []);
+
+    return (
         <MainLayout>
             <section>
-                
+                {productos.map((producto) => (
+                    <li key={producto.id}>
+                        {producto.id} - {producto.nombre} - {producto.unidad}
+                    </li>
+                ))}
             </section>
         </MainLayout>
     );
