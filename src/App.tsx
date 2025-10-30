75c5847435cc6d2a02ee291b37557cc468f0a91c
BersainCortes <bersaincortes10@gmail.com>
2025-10-29 22:15:51 -0600

    add new path

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './pages/home'
 import Products from './pages/products'
+import InfoProduct from './pages/infoProduct'
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
             <Routes>
                 <Route path='/' element={<Home></Home>}></Route>
                 <Route path='/productos' element={<Products></Products>}></Route>
+                <Route path='/productos/:id' element={<InfoProduct></InfoProduct>}></Route>
             </Routes>
         </Router>
     )
